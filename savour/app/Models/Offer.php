67ef0fac0a0bf58e77047cb07b743600285a4d7c
Afonso Morais <afonsopalmamorais@gmail.com>
2022-12-15 02:07:20 -0800

    Offer controller and model added

---
+++ savour/app/Models/Offer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    use HasFactory;
+
+    /*
+        The model will automatically connect/link to the correct table.
+        To do this, "Eloquent" uses snake case: name of the class, lower case, plural.
+
+        If you are using a different name, add this property to the model :
+            protected $table = 'my_table';
+
+        By default, Laravel assume that the primary key is named "id"
+        If you are using a different name, add this property :
+            protected $primaryKey = 'my_id';
+            
+            */
+
+    // Accessors & Mutators (setters & getters)
+    // protected function name(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn ($value) => strtoupper($value),
+    //         set: fn ($value) => strtolower($value),
+    //     );
+    // }
+
+    // protected function priceWithEuro(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn () => $this->attributes['price'] . ' €',
+    //     );
+    // }
+
+    public function getPriceWithEuroAttribute()
+    {
+        return $this->attributes['price'] . ' €';
+    }
+
+    public function getCreatedAtAttribute($value)
+    {
+        return date('d M Y', strtotime($value));
+    }
+
+    public function getNameAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = strtolower($value);
+    }
+}
--- savour/routes/web.php
+++ savour/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\OfferController;
 use Illuminate\Support\Facades\Route;
 use Termwind\Components\Raw;
 
@@ -44,30 +45,15 @@ Route::middleware('auth:restaurant')->prefix('restaurant')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+// API ROUTES FOR OFFERS
+
+Route::get('/api/offers/name={name}', [OfferController::class, 'offer_name']);
+Route::get('/api/offers/restaurant_id={id}', [OfferController::class, 'all_from_restaurant']);
+Route::get('/api/offers/active', [OfferController::class, 'all_active']);
+Route::get('/api/offers/active/restaurant_id={id}', [OfferController::class, 'all_active_from_restaurant']);
+Route::get('/api/offers/inactive/restaurant_id={id}', [OfferController::class, 'all_inactive_from_restaurant']);
+Route::get('/api/offers/last', [OfferController::class, 'last_added_offers']);
 
 
 
 
-
-
-
-
-
-/* Route::prefix('customer')->group(function() { // ROUTES GROUP FOR CUSTOMERS
-    // LOGIN ROUTE FOR CUSTOMERS
-    Route::get('/login', [CustomerContoller::class, 'login_form']);
-    Route::post('/login', [CustomerContoller::class, 'login']);
-    // REGISTER ROUTES FOR CUSTOMERS
-    Route::get('/register', [CustomerContoller::class, 'register_form']);
-    Route::post('/register', [CustomerContoller::class, 'register']);
-
-});
-
-Route::prefix('restaurant')->group(function() { // ROUTES GROUP FOR RESTAURANTS
-    // LOGIN ROUTE FOR RESTAURANTS
-    Route::get('/login', [RestaurantContoller::class, 'login_form']);
-    Route::post('/login', [RestaurantContoller::class, 'login']);
-    // REGISTER ROUTES FOR RESTAURANTS
-    Route::get('/register', [RestaurantContoller::class, 'register_form']);
-    Route::post('/register', [RestaurantContoller::class, 'register']);
-}); */
