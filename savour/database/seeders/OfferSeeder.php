afd0f2b2ffa111500ade1c1ac2c3c24e9849e075
ning <lee.ninghsin@gmail.com>
2022-12-20 11:04:33 +0100

    added seeder and navbar icon

--- savour/database/seeders/OfferSeeder.php
+++ savour/database/seeders/OfferSeeder.php
@@ -25,7 +25,7 @@ class OfferSeeder extends Seeder
                 'quantity' => '5',
                 'price' => '12',
                 'is_active' => '1',
-                'restaurant_id' => '1',
+
             ],
             [
                 'name' => 'Tomyam Koong',
@@ -35,7 +35,7 @@ class OfferSeeder extends Seeder
                 'quantity' => '6',
                 'price' => '18',
                 'is_active' => '1',
-                'restaurant_id' => '4',
+
             ],
             [
                 'name' => 'Beef noodles',
@@ -45,7 +45,152 @@ class OfferSeeder extends Seeder
                 'quantity' => '14',
                 'price' => '23',
                 'is_active' => '1',
+
+            ],
+            [
+
+                'name' => 'ShepherdsPie',
+
+                'restaurant_id' => '4',
+
+                'description' => 'minced lamb with potatoes and Worcestershire sauce',
+
+                'image' => ' Shepherds-Pie.jpg',
+
+                'quantity' => '14',
+
+                'price' => '12.50',
+
+                'is_active' => '1',
+
+            ],
+
+            [
+
+                'name' => 'Chilli con carne',
+
                 'restaurant_id' => '3',
+
+                'description' => 'lean minced beef, 2 medium onions, 3 garlic cloves, hot chilli powder ',
+
+                'image' => ' chilli.jpg',
+
+                'quantity' => '10',
+
+                'price' => '10',
+
+                'is_active' => '1',
+
+            ],
+
+            [
+
+                'name' => 'Tacos',
+
+                'restaurant_id' => '3',
+
+                'description' => 'Beef, Onion, Garlic, Chilli powder, Oregano ',
+
+                'image' => ' tacos.jpg',
+
+                'quantity' => '30',
+
+                'price' => '8',
+
+                'is_active' => '1',
+
+            ],
+
+
+
+
+
+
+
+            [
+
+                'name' => 'Bolognese',
+
+                'restaurant_id' => '2',
+
+                'description' => 'Pasta of your choice, Red wine, Beef mince, cherry tomatoes, onions, garlic, tomato sauce ',
+
+                'image' => ' bolognese.jpg',
+
+                'quantity' => '15',
+
+                'price' => '9',
+
+                'is_active' => '1',
+
+            ],
+
+            [
+
+                'name' => 'Sweet and sour chicken',
+
+                'restaurant_id' => '1',
+
+                'description' => ' Sweet and Sour Chicken with crispy chicken, pineapple and bell peppers ',
+
+                'image' => ' Sweet-and-Sour-Chicken.jpg',
+
+                'quantity' => '12',
+
+                'price' => '9.50',
+
+                'is_active' => '1',
+
+            ],
+
+            [
+
+                'name' => 'tomahawk steak',
+
+                'restaurant_id' => '5',
+
+                'description' => ' tomahawk rib-eye steak, salt',
+
+                'image' => ' tomahawk-rib-eye-steak.jpg',
+
+                'quantity' => '12',
+
+                'price' => '60.50',
+
+                'is_active' => '1',
+
+
+            ],
+
+
+
+
+
+
+
+
+
+
+
+
+
+            [
+
+                'name' => 'Carbonara',
+
+                'restaurant_id' => '2',
+
+                'description' => ' Pasta of your choice, bacon, onions, cream, parmesan,',
+
+                'image' => ' Carbonara.jpg',
+
+                'quantity' => '19',
+
+                'price' => '8.50',
+
+                'is_active' => '1',
+
+
             ],
         ]);
     }
